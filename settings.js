2e7cf58df29bcb84af4c1d4c09657b0f2c6c3e97
confrontertech <142077542+confronter@users.noreply.github.com>
2024-06-14 08:36:19 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,29 +19,28 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
+global.botname = 'DEMON KILLER' //ur bot name
+global.ownernumber = '254796283064' //ur owner number
+global.ownername = 'CONFRONTER' //ur owner name
+global.websitex = "https://www.instagram.com/confronter._?igsh=MXI5dmtmd2ZpYmM2Nw=="
+global.wagc = "https://whatsapp.com/channel/0029Vag3MeuGJP8LZb1Okj39"
+global.wm = "DEMON KILLER"
+global.botscript = 'FORGET ABOUT IT DUDE' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "💀Confronter\n\n+254796283064"
+global.creator = "254796283064@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["254796283064"] // Premium User
 global.hituet = 0
 
 //bot sett
-global.typemenu = 'v8' // menu type 'v1' => 'v8'
+global.typemenu = 'v4' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
